02358f3303c9b9d32726318cdde65e9bb8cd1e6e
Ateeb-Karim <ateebkarim.ak02@gmail.com>
2026-08-23 22:12:28 +0500

    updated files

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ export default function MarketingPage() {
     <>
       <Navbar />
       <div className="flex flex-col justify-center items-center min-h-screen bg-muted">
-        <h1>hello world</h1>
+        <h1>hello </h1>
       </div>
     </>
   );
