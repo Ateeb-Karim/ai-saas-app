703f6df663485038e15ace17d3f7dd313b820329
Ateeb-Karim <ateebkarim.ak02@gmail.com>
2026-08-26 02:35:09 +0500

    dealing with proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
-import { authConfig } from "./auth.config";
 import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 
-const { auth: middleware } = NextAuth(authConfig);
+export const { auth: proxy } = NextAuth(authConfig);
 
 export const config = {
   matcher: ["/dashboard/:path*"],
