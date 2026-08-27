bb462d03ff10ea62c8fdfd48984f928d42481352
Ateeb-Karim <ateebkarim.ak02@gmail.com>
2026-08-27 17:20:07 +0500

    working on the dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,9 +12,9 @@ export default function DashboardLayout({
       <aside className="w-full flex-none md:block hidden md:w-64 bg-[#12161F] text-[#F5F6F8] shrink-0 flex-col h-screen">
         <SideBar />
       </aside>
-      <div className="flex flex-col grow w-full overflow-y-auto md:rounded-2xl">
+      <div className="flex flex-col grow w-full overflow-y-auto md:overflow-hidden md:rounded-2xl">
         <Topbar />
-        <section className="flex flex-col items-center justify-center p-6 grow">
+        <section className="flex flex-col items-center justify-center p-6 grow overflow-hidden md:overflow-visible">
           {children}
         </section>
       </div>
