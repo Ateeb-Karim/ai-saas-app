7c2236b0d2eee8b2bd085a7a197d419909821d21
Ateeb-Karim <ateebkarim.ak02@gmail.com>
2026-09-01 00:16:40 +0500

    initializing image generator

--- app/api/generate-image/route.ts
+++ app/api/generate-image/route.ts
@@ -1,51 +1,23 @@
-import { GoogleGenAI } from "@google/genai";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
+export async function POST(request: Request) {
   try {
-    const { prompt } = await req.json();
+    const { prompt } = await request.json();
+
     if (!prompt) {
       return NextResponse.json(
-        { error: "Prompt is required" },
+        { error: "prompt is required" },
         { status: 400 },
       );
     }
 
-    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-    const response = await ai.models.generateContent({
-      model: "gemini-3.5-flash-image",
-      contents: prompt,
-    });
+    const imageUrl = `https://image.pollinations.ai/prompt/${encodeURIComponent(
+      prompt,
+    )}`;
 
-    const parts = response.candidates?.[0]?.content?.parts;
-    if (!parts || !Array.isArray(parts)) {
-      return NextResponse.json(
-        { error: "Invalid response from AI" },
-        { status: 500 },
-      );
-    }
-
-    let imagebase64: string | null = null;
-
-    for (const part of parts) {
-      if (
-        part?.inlineData &&
-        part?.inlineData?.data &&
-        part?.inlineData?.mimeType
-      ) {
-        imagebase64 = part.inlineData.data || null;
-      }
-    }
-
-    if (!imagebase64) {
-      return NextResponse.json(
-        { error: "No image data received from AI" },
-        { status: 500 },
-      );
-    }
-
-    return NextResponse.json({ image: imagebase64 });
-  } catch (e) {
+    return NextResponse.json({ imageUrl: imageUrl });
+  } catch (error) {
+    console.error("Error generating image:", error);
     return NextResponse.json(
       { error: "Failed to generate image" },
       { status: 500 },
