97970c33841eb149cd655d4cdb9995db77a6e3cd
Ateeb-Karim <ateebkarim.ak02@gmail.com>
2026-09-07 00:15:26 +0500

    working on profile page

--- app/(dashboard)/profile/edit/page.tsx
+++ app/(dashboard)/profile/edit/page.tsx
@@ -1,11 +1,40 @@
-import { JSX } from "react";
+"use client";
+
+import { JSX, useEffect, useState } from "react";
 import Link from "next/link";
 import ShowCard from "../../_components/showcard";
 import { User, ArrowLeft } from "lucide-react";
-import { auth } from "@/auth";
+import { useSession } from "next-auth/react";
 
-export default async function EditProfilePage(): Promise<JSX.Element> {
-  const session = await auth();
+export default function EditProfilePage(): JSX.Element {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session?.user) {
+      setFormData({
+        name: session.user.name || "",
+        email: session.user.email || "",
+      });
+    }
+  }, [session]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <div className="flex flex-col gap-5 items-start w-full h-full text-[#F5F6F8] px-4 sm:px-0">
@@ -36,7 +65,51 @@ export default async function EditProfilePage(): Promise<JSX.Element> {
           <span className="text-sm">back to dashboard</span>
         </Link>
       </div>
-      <div className="w-full"></div>
+
+      <div className="w-full p-5 border border-[#2A2F3A] rounded-lg">
+        <div className="w-full flex flex-col border-b border-[#2A2F3A] pb-5">
+          <h3 className="text-xl font-bold text-[#F5F6F8]">Edit Profile</h3>
+          <p className="text-[#8B93A5] text-sm">
+            Manage your profile and account settings
+          </p>
+        </div>
+        <form onSubmit={handleSubmit} className="w-full flex flex-col mt-2">
+          <div className="w-full flex flex-col">
+            <label htmlFor="name" className="text-[#F5F6F8] text-lg">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-[#12161F] border border-[#2A2F3A] px-3 py-2 rounded-md cursor-pointer capitalize outline-none"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="w-full">
+            <label htmlFor="email" className="text-[#F5F6F8] text-lg">
+              password
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-[#12161F] border border-[#2A2F3A] px-3 py-2 rounded-md cursor-pointer capitalize outline-none"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="w-full flex gap-5 mt-5 justify-end">
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded-md cursor-pointer hover:bg-blue-600 hover:scale-105 transition-all capitalize"
+            >
+              Save Changes
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
