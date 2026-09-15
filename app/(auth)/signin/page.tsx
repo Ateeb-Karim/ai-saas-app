8b66949183f01e2fb00c1ef7e19efd18d0c8dbb5
Ateeb-Karim <ateebkarim.ak02@gmail.com>
2026-09-16 00:03:27 +0500

    updating the sign in page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -6,15 +6,21 @@ import { useRouter } from "next/navigation";
 import { JSX, useState } from "react";
 import toast from "react-hot-toast";
 import { signIn } from "next-auth/react";
+import bcrypt from "bcryptjs";
+import { Loader2 } from "lucide-react";
+import { prisma } from "@/lib/prisma";
 
 export default function SigninPage(): JSX.Element {
-  const router = useRouter();
+  const [emailValid, setEmailValid] = useState<boolean>(false);
+  const [passwordValid, setPasswordValid] = useState<boolean>(false);
+  const [isloading, setIsLoading] = useState<boolean>(false);
 
   const [formData, setFormData] = useState<signInFormType>({
     email: "",
     password: "",
   });
-  const [emailValid, setEmailValid] = useState<boolean>(false);
+
+  const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -25,15 +31,104 @@ export default function SigninPage(): JSX.Element {
     return email.includes("@") && email.includes(".");
   };
 
+  const PasswordChecking = async (password: string): Promise<boolean> => {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: formData.email,
+      },
+    });
+
+    if (!user || !user.password) return false;
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    return passwordMatch;
+  };
+
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
+    try {
+      const isEmailValid: boolean = EmailChecking(formData.email);
 
-    const isEmailValid: boolean = EmailChecking(formData.email);
+      if (!isEmailValid) {
+        toast.error("Invalid email", {
+          icon: "❌",
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+          position: "top-center",
+          duration: 3000,
+        });
 
-    console.log(isEmailValid);
+        setEmailValid((prev) => !prev);
+        setFormData({ email: "", password: "" });
+        return;
+      }
 
-    if (!isEmailValid) {
-      toast.error("Invalid email", {
+      const isPasswordCorrect = await PasswordChecking(formData.password);
+
+      if (!isPasswordCorrect) {
+        console.log(isPasswordCorrect);
+        toast.error("Invalid password", {
+          icon: "❌",
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+          position: "top-center",
+          duration: 3000,
+        });
+
+        setPasswordValid((prev) => !prev);
+        setFormData({ email: "", password: "" });
+
+        return;
+      }
+
+      const result = await signIn("credentials", {
+        email: formData.email,
+        password: formData.password,
+
+        redirect: false,
+      });
+
+      if (!result?.ok) {
+        toast.error("Invalid credentials", {
+          icon: "❌",
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+          position: "top-center",
+          duration: 3000,
+        });
+
+        setFormData({ email: "", password: "" });
+
+        return;
+      }
+
+      toast.success("Login successful", {
+        icon: "✅",
+        style: {
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
+        },
+        position: "top-center",
+        duration: 3000,
+      });
+
+      setIsLoading(false);
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to log in", {
         icon: "❌",
         style: {
           borderRadius: "10px",
@@ -43,45 +138,7 @@ export default function SigninPage(): JSX.Element {
         position: "top-center",
         duration: 3000,
       });
-      setEmailValid((prev) => !prev);
-      return;
     }
-
-    const result = await signIn("credentials", {
-      email: formData.email,
-      password: formData.password,
-      redirect: false,
-    });
-
-    if (!result?.ok) {
-      toast.error("Invalid credentials", {
-        icon: "❌",
-        style: {
-          borderRadius: "10px",
-          background: "#333",
-          color: "#fff",
-        },
-        position: "top-center",
-        duration: 3000,
-      });
-
-      setFormData({ email: "", password: "" });
-
-      return;
-    }
-
-    toast.success("Login successful", {
-      icon: "✅",
-      style: {
-        borderRadius: "10px",
-        background: "#333",
-        color: "#fff",
-      },
-      position: "top-center",
-      duration: 3000,
-    });
-
-    router.push("/dashboard");
   };
 
   return (
@@ -129,15 +186,28 @@ export default function SigninPage(): JSX.Element {
           value={formData.password}
           onChange={handleChange}
           placeholder="Enter your password"
-          className="w-full border border-gray-500 rounded-md px-3 py-2 outline-none"
+          className={`w-full border ${passwordValid ? "border-red-500" : "border-gray-500"} rounded-md px-3 py-2 outline-none`}
           required
         />
+        {passwordValid && (
+          <p className="text-sm text-center text-red-500">
+            Invalid credentials
+          </p>
+        )}
       </div>
       <button
         type="submit"
-        className="w-full bg-blue-500 text-center px-4 py-2 rounded-md font-semibold text-white cursor-pointer hover:bg-blue-600 transition-colors duration-200 ease-in-out"
+        disabled={isloading}
+        className="w-full bg-blue-500 text-center px-4 py-2 rounded-md font-semibold text-white cursor-pointer hover:bg-blue-600 transition-colors duration-200 ease-in-out flex items-center justify-center gap-2 active:scale-95"
       >
-        Sign In
+        {isloading ? (
+          <>
+            <Loader2 className="animate-spin" />
+            <span>Signing in...</span>
+          </>
+        ) : (
+          <span>Sign In</span>
+        )}
       </button>
       <p className="text-sm text-center text-muted-foreground">
         Don&apos;t have an account?{" "}
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
+      data-scroll-behavior="smooth"
     >
       <body className="min-h-full flex flex-col">
         <Provider>{children}</Provider>
